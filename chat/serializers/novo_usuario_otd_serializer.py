0af9681c50e85afcb46497b3fb7923e5f00542f7
fr-mm <chico.zimcolor@gmail.com>
2023-01-21 18:15:44 -0300

    mensagens de erro na criação de usuario são enviadas

--- chat/serializers/novo_usuario_otd_serializer.py
+++ chat/serializers/novo_usuario_otd_serializer.py
@@ -1,6 +1,36 @@
+from dataclasses import dataclass
+
 from rest_framework import serializers
 
 
+@dataclass(frozen=True)
+class Limites:
+    tamanho_minimo: int
+    tamanho_maximo: int
+
+
+@dataclass(frozen=True, init=False)
+class LimitesDeAtributos:
+    nome = Limites(
+        tamanho_minimo=3,
+        tamanho_maximo=50
+    )
+    senha = Limites(
+        tamanho_minimo=4,
+        tamanho_maximo=50
+    )
+
+
 class NovoUsuarioOTDSerializer(serializers.Serializer):
-    nome = serializers.CharField(min_length=3, max_length=50)
-    senha = serializers.CharField(min_length=6, max_length=50)
+    nome = serializers.CharField(
+        min_length=LimitesDeAtributos.nome.tamanho_minimo,
+        max_length=LimitesDeAtributos.nome.tamanho_maximo
+    )
+    senha = serializers.CharField(
+        min_length=LimitesDeAtributos.senha.tamanho_minimo,
+        max_length=LimitesDeAtributos.senha.tamanho_maximo
+    )
+
+    @classmethod
+    def limites(cls) -> LimitesDeAtributos:
+        return LimitesDeAtributos()
